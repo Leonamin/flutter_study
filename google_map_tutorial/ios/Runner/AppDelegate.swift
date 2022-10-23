a747148748e4e1525d975a4ac40ebce0f8cf9eef
Leonamin <yism810@gmail.com>
2022-10-23 21:11:01 +0900

    추가 - 구글 맵스 iOS 설정 정상 동작 확인필요

--- google_map_tutorial/ios/Runner/AppDelegate.swift
+++ google_map_tutorial/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if let googleMapsApiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API"] {
+      GMSServices.provideAPIKey(googleMapsApiKey)        
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
