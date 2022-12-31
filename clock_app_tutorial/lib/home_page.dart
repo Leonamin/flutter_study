d6fbfc2bcd9790250873b46537ff31f962814b68
Leonamin <51023129+Leonamin@users.noreply.github.com>
2022-12-31 14:48:04 +0900

    추가 - 좀 더 풍부하게 보이도록...

--- clock_app_tutorial/lib/home_page.dart
+++ clock_app_tutorial/lib/home_page.dart
@@ -1,18 +1,99 @@
 import 'package:clock_app_tutorial/clock_view.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split('.').first;
+    var offsetSign = '-';
+    if (!timezoneString.startsWith('-')) {
+      offsetSign = '+';
+    }
+
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Container(
-        alignment: Alignment.center,
-        child: ClockView(),
+      body: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: () {},
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FlutterLogo(),
+                      Text(
+                        'clock',
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+          const VerticalDivider(
+            color: Colors.white54,
+            width: 1,
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 64,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Clock',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    formattedTime,
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                  Text(
+                    formattedDate,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  ClockView(),
+                  Text(
+                    'Timezone',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.language,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        'UTC$offsetSign$timezoneString',
+                        style: Theme.of(context).textTheme.labelSmall,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
