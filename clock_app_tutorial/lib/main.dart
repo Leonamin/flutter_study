14a24c675f85f4f9dba509ebbf13a7718f45f801
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-01 22:21:28 +0900

    디버그 - 아이콘 오류

--- clock_app_tutorial/lib/main.dart
+++ clock_app_tutorial/lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
 
   // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
   const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('flutter_icon');
+      AndroidInitializationSettings('@mipmap/ic_launcher');
   final DarwinInitializationSettings initializationSettingsDarwin =
       DarwinInitializationSettings(
     onDidReceiveLocalNotification: (id, title, body, payload) async {},
